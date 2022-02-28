f215fe451607bc654b0b4f5fe0670c881b3ab323
cristiann190 <cristiann190@gmail.com>
2022-02-28 20:53:41 +0100

    Test null cases

--- test/src/SheepCounterTest.java
+++ test/src/SheepCounterTest.java
@@ -12,9 +12,27 @@ class SheepCounterTest {
 			true, true, true, true,
 			false, false, true, true};
 
+	Boolean[] arrayNullItems = {true, true, null, false,
+			true, true, true, true,
+			true, false, true, false,
+			true, false, null, true,
+			true, true, true, true,
+			false, false, true, true};
+
+	Boolean[] arrayNulll = null;
+
 	@Test
 	public void test() {
 		assertEquals(SheepCounter.countSheeps(array1), 17, "There are 17 sheeps in total");
 	}
 
+	@Test
+	public void testWithNullArray() {
+		assertEquals(SheepCounter.countSheeps(arrayNulll), 0, "There are 0 sheeps in total");
+	}
+
+	@Test
+	public void testWithNullItems() {
+		assertEquals(SheepCounter.countSheeps(arrayNullItems), 16, "There are 16 sheeps in total");
+	}
 }
